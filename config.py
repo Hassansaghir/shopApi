ddf81b4d465c19a1e92de3787b7f28f8cb21d851
Hassansaghir <98264816+Hassansaghir@users.noreply.github.com>
2024-09-14 15:50:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,7 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://@DESKTOP-4ID545F/clothesShop?driver=ODBC+Driver+17+for+SQL+Server&Trusted_Connection=yes'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+#If you're using Windows Authentication,
+you might not need to specify a username and password in the connection string. 
+Instead, you can use Trusted_Connection=yes in your connection string.
